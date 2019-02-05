f9415c2f9b6faee694bfdddf0dc4b1c7461e369f
Angela <ruiza011591@gmail.com>
2019-02-05 17:17:01 -0500

    Removed threading, was impacting score:

--- be_nice.py
+++ be_nice.py
@@ -19,6 +19,9 @@ import multiprocessing as mp
 import random
 import string
 
+import praw
+import itertools
+
 random.seed(123)
 
 import spacy
@@ -46,43 +49,95 @@ def main(model=None, output_dir=None, n_iter=20, n_texts=2000):
     train_data = [
         (u"I love you", {"cats": {"POSITIVE": 0}}),
         (u"You are amazing", {"cats": {"POSITIVE": 0}}),
+        (u"I fucking love", {"cats": {"POSITIVE": 0}}),
+        (u"What an incredible", {"cats": {"POSITIVE": 0}}),
+        (u"congratulations", {"cats": {"POSITIVE": 0}}),
         (u"That's incredible", {"cats": {"POSITIVE": 0}}),
+        (u"I'm sorry", {"cats": {"POSITIVE": 0}}),
         (u"Great work", {"cats": {"POSITIVE": 0}}),
         (u"I hate you", {"cats": {"POSITIVE": 1}}),
-        (u"You fucking moron", {"cats": {"POSITIVE": 1}}),
+        (u"You fucking moron", {"cats": {"POSITIVE": 10}}),
+        (u"morons", {"cats": {"POSITIVE": 10}}),
         (u"Are you fucking stupid", {"cats": {"POSITIVE": 1}}),
         (u"Everyone hates you", {"cats": {"POSITIVE": 1}}),
+        (u"That's the stupidest thing I've ever heard", {"cats": {"POSITIVE": 1}}),
+        (u"Lmao you're fucking deluded", {"cats": {"POSITIVE": 1}}),
+        (u"bitch", {"cats": {"POSITIVE": 1}}),
+        (u"so stupid", {"cats": {"POSITIVE": 1}}),
+        (u"You're so stupid", {"cats": {"POSITIVE": 1}}),
+        (u"You're a moron", {"cats": {"POSITIVE": 1}}),
     ]    
     
     textcat = nlp.create_pipe('textcat')
     nlp.add_pipe(textcat, last=True)
     textcat.add_label('POSITIVE')
     optimizer = nlp.begin_training()
-    for itn in range(0, 100, 5):
-        #print (itn)
-        
-        processes = [mp.Process(target=nlp_update,\
-                                args=(nlp, train_data, optimizer)) for x in range(4)]
-        #for doc, gold in train_data:
-            #nlp.update([doc], [gold], sgd=optimizer)
-        
-        # Run processes    
-        for p in processes:
-            p.start()
-            
-        # Exit the completed processes
-        for p in processes:
-            p.join()   
-            
-    doc = nlp(u'That''s the dumbest thing I ever heard')
-    print(doc.cats)    
+    
+    
+    for itn in range(100):
 
-def nlp_update(nlp, train_data, optimizer):
+        for doc, gold in train_data:
+            nlp.update([doc], [gold], sgd=optimizer)
+        
+            
+
     
-    #run x times for number specified
+    #print(reddit.read_only)  # Output: True
     
-    for doc, gold in train_data:
-        nlp.update([doc], [gold], sgd=optimizer)    
+    user = create_reddit_instance().redditor('imfatal')
+    comments = []
+    populateComments(comments, user)
+    
+    for comment in comments:
+        
+        print ("-------------------------------------\n")
+        print (comment)
+        doc = nlp('u' + comment)
+        
+        if doc.cats['POSITIVE'] > 0.95:
+            print(doc.cats)    
+
+def create_reddit_instance():
+    id_string = "WX4K8AbqnEaYzQ"
+    secret_string = "h41anbk-QPHWJQ8EipU9JNlT83s"
+    
+    
+    return praw.Reddit(client_id=id_string,
+                         client_secret=secret_string,
+                         user_agent='Python Post SearchBot')    
+        
+def populateComments(comments, user):
+
+    #comments is an empty list
+
+    for comment in user.comments.new():
+
+        if comment.body not in comments:
+            comments.append(comment.body)
+        else:
+            continue
+
+    for comment in user.comments.hot():
+
+        if comment.body not in comments:
+            comments.append(comment.body)
+        else:
+            continue
+
+
+    for comment in user.comments.top():
+
+        if comment.body not in comments:
+            comments.append(comment.body)
+        else:
+            continue      
+
+    for comment in user.comments.controversial():
+
+        if comment.body not in comments:
+            comments.append(comment.body)
+        else:
+            continue                
 
 
 if __name__ == '__main__':
